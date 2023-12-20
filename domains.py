d073b3edb99b2a22f7ba2af9771d21f59cb36bc7
ubernaut <collin.schroeder@gmail.com>
2023-12-20 13:46:21 -0900

    multithreaded

--- domains.py
+++ domains.py
@@ -1,6 +1,10 @@
 import os
-import whois
 import sys
+from multiprocessing import Pool
+
+import pandas as pd
+from tqdm import tqdm
+from whois import whois
 
 def load_words():
     with open('words.txt', 'r') as f:
@@ -27,20 +31,41 @@ def find_domains(words, tlds):
 def write_matches_to_file(matches, filename='matches.txt'):
     with open(filename, 'w') as f:
         for match in matches:
-            f.write(match + '\n')
+            print(match)
+            if type(match) is tuple:
+                f.write(str(match[0])+" "+str(match[1])+"\n")
+            else:
+                f.write(match + '\n')
+
+# def check_domain(domain):
+#     try:
+#         # Get the WHOIS information for the domain
+#         w = whois.whois(domain)
+#         if w.status == "free":
+#             return True
+#         else:
+#             return False
+#     except Exception as e:
+#         print("Error: ", e)
+#         print(domain+" had an issue")
+#         return False
+def blockPrint():
+    sys.stdout = open(os.devnull, "w")
+
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
 
 def check_domain(domain):
     try:
-        # Get the WHOIS information for the domain
-        w = whois.whois(domain)
-        if w.status == "free":
-            return True
-        else:
-            return False
-    except Exception as e:
-        print("Error: ", e)
-        print(domain+" had an issue")
-        return False
+        blockPrint()
+        result = whois(domain)
+    except:
+        return domain, None
+    finally:
+        enablePrint()
+    return domain, result.status
 
 def check_available(matches):
     print('checking availability')
@@ -57,5 +82,16 @@ if __name__ == '__main__':
     matches = find_domains(words, tlds)
     matches = sorted(matches, key=len, reverse=False)  # Sort the matches list by length in descending order
     write_matches_to_file(matches)
-    available = check_available(matches)
-    write_matches_to_file(available, filename="available.txt")
+    domains = matches
+
+    results = []
+    with Pool(processes=32) as pool:  # <-- select here how many processes do you want
+        for domain, status in tqdm(
+            pool.imap_unordered(check_domain, domains), total=len(domains)
+        ):
+            results.append((domain, not bool(status)))
+
+    df = pd.DataFrame(results, columns=["domain", "is_free"])
+    print(df.drop_duplicates())
+
+    write_matches_to_file(results, filename="results.txt")
